7085d443e6014e313b0185b50a9d846361370c4d
Nicki <nicki@dewet.org.za>
2020-10-06 21:27:12 +0200

    first commit

---
+++ FormModelRunner.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ModelRunner
+{
+    partial class FormModelRunner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDataSource = new System.Windows.Forms.Label();
+            this.textBoxDataSource = new System.Windows.Forms.TextBox();
+            this.labelModel = new System.Windows.Forms.Label();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.labelResults = new System.Windows.Forms.Label();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelDataSource
+            // 
+            this.labelDataSource.AutoSize = true;
+            this.labelDataSource.Location = new System.Drawing.Point(13, 13);
+            this.labelDataSource.Name = "labelDataSource";
+            this.labelDataSource.Size = new System.Drawing.Size(88, 13);
+            this.labelDataSource.TabIndex = 0;
+            this.labelDataSource.Text = "Data Source (xls)";
+            // 
+            // textBoxDataSource
+            // 
+            this.textBoxDataSource.Location = new System.Drawing.Point(108, 13);
+            this.textBoxDataSource.Name = "textBoxDataSource";
+            this.textBoxDataSource.Size = new System.Drawing.Size(332, 20);
+            this.textBoxDataSource.TabIndex = 1;
+            // 
+            // labelModel
+            // 
+            this.labelModel.AutoSize = true;
+            this.labelModel.Location = new System.Drawing.Point(13, 43);
+            this.labelModel.Name = "labelModel";
+            this.labelModel.Size = new System.Drawing.Size(36, 13);
+            this.labelModel.TabIndex = 2;
+            this.labelModel.Text = "Model";
+            // 
+            // textBoxModel
+            // 
+            this.textBoxModel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxModel.Location = new System.Drawing.Point(108, 40);
+            this.textBoxModel.Name = "textBoxModel";
+            this.textBoxModel.Size = new System.Drawing.Size(332, 20);
+            this.textBoxModel.TabIndex = 3;
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(108, 67);
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(332, 20);
+            this.textBoxResults.TabIndex = 4;
+            // 
+            // labelResults
+            // 
+            this.labelResults.AutoSize = true;
+            this.labelResults.Location = new System.Drawing.Point(12, 70);
+            this.labelResults.Name = "labelResults";
+            this.labelResults.Size = new System.Drawing.Size(42, 13);
+            this.labelResults.TabIndex = 5;
+            this.labelResults.Text = "Results";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(108, 94);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(100, 23);
+            this.buttonRun.TabIndex = 6;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(13, 133);
+            this.textBoxOutput.MaxLength = 100000;
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(528, 304);
+            this.textBoxOutput.TabIndex = 7;
+            // 
+            // FormModelRunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 449);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.labelResults);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.textBoxModel);
+            this.Controls.Add(this.labelModel);
+            this.Controls.Add(this.textBoxDataSource);
+            this.Controls.Add(this.labelDataSource);
+            this.Name = "FormModelRunner";
+            this.Text = "ModelRunner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDataSource;
+        private System.Windows.Forms.TextBox textBoxDataSource;
+        private System.Windows.Forms.Label labelModel;
+        private System.Windows.Forms.TextBox textBoxModel;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Label labelResults;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.TextBox textBoxOutput;
+    }
+}
+
---
+++ FormModelRunner.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ModelRunner
+{
+    public partial class FormModelRunner : Form
+    {
+        public FormModelRunner()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            string dataFileName = textBoxDataSource.Text + ".txt";
+            string lpFileName = textBoxDataSource.Text + ".lp";
+            string resultsFileName = textBoxDataSource.Text + ".results.txt";
+
+            textBoxOutput.Clear();
+
+            Cursor.Current = Cursors.WaitCursor;
+
+            try
+            {
+                ExtractDataFromXLS(textBoxDataSource.Text, dataFileName);
+                RunGLPSOL(dataFileName, textBoxModel.Text, lpFileName);
+                RunCBC(lpFileName, resultsFileName);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Error Running Model");
+            }
+            finally
+            {
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private bool ExtractDataFromXLS(string xlsFileName, string dataFileName)
+        {
+            return true;
+        }
+        private bool RunGLPSOL(string dataFileName, string modelFileName, string lpFileName)
+        {
+            return RunProcess("utils\\glpsol.exe", string.Format("--check -m {0} -d {1} --wlp {2}", modelFileName, dataFileName, lpFileName));
+        }
+
+        private bool RunCBC(string inputFileName, string outputFileName)
+        {
+            return RunProcess("utils\\cbc.exe", String.Format("{0} solve -solu {1}", inputFileName, outputFileName));
+        }
+
+        private bool RunProcess(string filename, string args)
+        {
+            try
+            {
+                textBoxOutput.Text += new string('-', 150) + "\r\n";
+                textBoxOutput.Text += string.Format("Running {0} {1}\r\n", filename, args);
+                textBoxOutput.Text += new string('-', 150) + "\r\n";
+                Process compiler = new Process();
+                compiler.StartInfo.FileName = filename;
+                compiler.StartInfo.Arguments = args;
+                compiler.StartInfo.UseShellExecute = false;
+                compiler.StartInfo.RedirectStandardOutput = true;
+                compiler.StartInfo.CreateNoWindow = false;
+
+                compiler.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                // Prepend line numbers to each line of the output.
+                if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        textBoxOutput.Text += e.Data;
+                    }
+                });
+
+                compiler.Start();
+
+                textBoxOutput.Text += compiler.StandardOutput.ReadToEnd();
+
+                compiler.WaitForExit();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                textBoxOutput.Text += "Error: " + e.Message + "\r\n";
+                return false;
+            }
+        }
+    }
+}
